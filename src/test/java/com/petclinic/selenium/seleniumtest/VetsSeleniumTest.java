4dadc64cbe79aa9261d68ebc096cc8fea90ce54b
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-10-18 12:40:14 -0400

    First selenium test implemented and working

---
+++ src/test/java/com/petclinic/selenium/seleniumtest/VetsSeleniumTest.java
@@ -0,0 +1,89 @@
+package com.petclinic.selenium.seleniumtest;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(SeleniumExtension.class)
+public class VetsSeleniumTest {
+    ChromeDriver driver;
+    private final String SCREENSHOTS = "./src/test/onDemandScreenshots";
+
+    public VetsSeleniumTest(ChromeDriver driver) {
+        this.driver = driver;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        System.setProperty("sel.jup.screenshot.at.the.end.of.tests", "whenfailure");
+        System.setProperty("sel.jup.screenshot.format", "png");
+        System.setProperty("sel.jup.output.folder", "./src/test/onFailureScreenshots");
+    }
+
+    public static void takeSnapShot(WebDriver webDriver, String fileWithPath) throws Exception {
+
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot = ((TakesScreenshot) webDriver);
+        //call getScreenshotAs method to create the actual image file
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        //Move image file to new destination
+        File DestFile = new File(fileWithPath);
+        //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    @Test
+    @DisplayName("test_filter-vets-all")
+    void test_filter_vets_all(TestInfo testInfo) throws Exception {
+        driver.get("http://localhost:8080");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.linkText("Veterinarians")).click();
+
+//        Select dropdown = new Select(driver.findElement(By.id("filterOption")));
+//        dropdown.selectByValue("All");
+
+        WebElement rows = driver.findElement(By.xpath("//table[@class='table table-striped']"));
+        List<WebElement> TotalRowsList = rows.findElements(By.tagName("tr"));
+        assertThat(TotalRowsList.size(), is(7));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @DisplayName("test_filter-vets-available")
+    void test_filter_vets_available(TestInfo testInfo) throws Exception {
+        driver.get("http://localhost:8080");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.linkText("Veterinarians")).click();
+
+//        Select dropdown = new Select(driver.findElement(By.id("filterOption")));
+//        dropdown.selectByValue("All");
+
+        WebElement rows = driver.findElement(By.xpath("//table[@class='table table-striped']"));
+        List<WebElement> TotalRowsList = rows.findElements(By.tagName("tr"));
+        assertThat(TotalRowsList.size(), is(7));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
