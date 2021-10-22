97bfcae27adbc9c876d93da63d3351e950069569
Christian C <christian.chitanu@hotmail.com>
2021-10-22 19:48:26 -0400

    crate Selenium test for login test helper test

---
+++ src/test/java/com/petclinic/selenium/SeleniumLoginTestHelperTest.java
@@ -0,0 +1,15 @@
+package com.petclinic.selenium;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SeleniumExtension.class)
+public class SeleniumLoginTestHelperTest {
+    @Test
+    @DisplayName("Selenium Login Test Helper Test")
+    public void seleniumLoginTestHelperTest(){
+
+    }
+}
