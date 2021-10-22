526223b4e0b1dea8a17da5248d5ddbb426b42841
Trim0500 <70395022+Trim0500@users.noreply.github.com>
2021-10-22 14:29:39 -0400

    bug(BILL-CPC-442): Fix History Page Routing (#3)
    * Initial commit. Test to check for billing history page passes
    
    * Ran a test on main for before screenshot

---
+++ src/test/java/com/petclinic/selenium/seleniumbillingservicetest/SeleniumBillingServiceTest.java
@@ -0,0 +1,67 @@
+package com.petclinic.selenium.seleniumbillingservicetest;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(SeleniumExtension.class)
+public class SeleniumBillingServiceTest {
+    ChromeDriver driver;
+    private final String SCREENSHOTS = "./src/test/onDemandBillServiceScreenshots";
+
+    public SeleniumBillingServiceTest(ChromeDriver driver) {
+        this.driver = driver;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        System.setProperty("sel.jup.screenshot.at.the.end.of.tests", "whenfailure");
+        System.setProperty("sel.jup.screenshot.format", "png");
+        System.setProperty("sel.jup.output.folder", "./src/test/onFailureScreenshots");
+    }
+
+    public static void takeSnapShot(WebDriver webDriver, String fileWithPath) throws Exception {
+
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot = ((TakesScreenshot) webDriver);
+        //call getScreenshotAs method to create the actual image file
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        //Move image file to new destination
+        File DestFile = new File(fileWithPath);
+        //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    @Test
+    @DisplayName("test_petclinic")
+    public void takeBillingServiceSnapshot(TestInfo testInfo) throws Exception {
+        driver.get("http://localhost:8080/#!/bills");
+        driver.manage().window().maximize();
+
+        WebElement billHistoryHeader = driver.findElement(By.className("titleOwner"));
+
+        String method = testInfo.getDisplayName();
+        takeSnapShot(driver, SCREENSHOTS + "\\" + method + "_" + System.currentTimeMillis() + ".png");
+
+        assertThat(billHistoryHeader.getText(), is("Bill History"));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
