804a46cee6d9ee8ff86d4683421b66783d3eb7e3
Christian C <christian.chitanu@hotmail.com>
2021-10-23 16:03:43 -0400

    added final responsive test for when screenwidth is 550px

--- src/test/java/com/petclinic/selenium/seleniumtest/vets/VetSeleniumTableResponsivenessTest.java
+++ src/test/java/com/petclinic/selenium/seleniumtest/vets/VetSeleniumTableResponsivenessTest.java
@@ -61,7 +61,6 @@ public class VetSeleniumTableResponsivenessTest {
         String method = testInfo.getDisplayName();
         boolean error = false;
         //assert
-        //open web page 768 550
         try {
             Dimension dimension = new Dimension(1920, 1080);
             helper.getDriver().manage().window().setSize(dimension);
@@ -106,7 +105,6 @@ public class VetSeleniumTableResponsivenessTest {
         String method = testInfo.getDisplayName();
         boolean error = false;
         //assert
-        //open web page 768 550
         try {
             Dimension dimension = new Dimension(768, 1080);
             helper.getDriver().manage().window().setSize(dimension);
@@ -132,6 +130,50 @@ public class VetSeleniumTableResponsivenessTest {
             assertThat(modal.isDisplayed(), is(true));
             assertThat(modalPhone.isDisplayed(), is(true));
             assertThat(modalEmail.isDisplayed(), is(false));
+
+        }catch (AssertionError e){
+            e.printStackTrace();
+            error = true;
+            throw new AssertionError(e);
+        }
+        finally {
+            if(error) {
+                takeSnapShot(driver, SCREENSHOTS+"/fail/"+method+"_"+System.currentTimeMillis()+".png");
+            }
+            helper.getDriver().quit();
+        }
+    }
+    @Test
+    @DisplayName("Test_Vet_Table_550pxWidth_Modal_and_Table_data")
+    public void testModalAndTableData550pxWidth(TestInfo testInfo) throws Exception{
+        String method = testInfo.getDisplayName();
+        boolean error = false;
+        //assert
+        try {
+            Dimension dimension = new Dimension(550, 1080);
+            helper.getDriver().manage().window().setSize(dimension);
+            WebDriverWait wait = new WebDriverWait(driver,10);
+            wait.until(ExpectedConditions.urlToBe("http://localhost:8080/#!/welcome"));
+            helper.getDriver().get("http://localhost:8080/#!/vets");
+            wait.until(ExpectedConditions.urlToBe("http://localhost:8080/#!/vets"));
+            WebElement phone = helper.getDriver().findElement(By.className("vet_phone"));
+            WebElement email = helper.getDriver().findElement(By.className("vet_email"));
+            WebElement modalPhone = helper.getDriver().findElement(By.className("modal_phone"));
+            WebElement modalEmail = helper.getDriver().findElement(By.className("modal_email"));
+            assertThat(phone.isDisplayed(), is(false));
+            assertThat(email.isDisplayed(), is(false));
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("info")));
+            Actions actions = new Actions(helper.getDriver());
+            WebElement info = helper.getDriver().findElement(By.className("info"));
+            actions.moveToElement(info);
+            actions.build().perform();
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("modalOn")));
+            WebElement modal = helper.getDriver().findElement(By.className("modalOn"));
+            assertThat(modal.isDisplayed(), is(true));
+            assertThat(modalPhone.isDisplayed(), is(true));
+            assertThat(modalEmail.isDisplayed(), is(true));
 
         }catch (AssertionError e){
             e.printStackTrace();
