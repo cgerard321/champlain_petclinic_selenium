faa7bcb0c50c24be7c20b8603efaa92c28565d1e
Louenes099 <islam.shariful@hotmail.com>
2021-10-23 05:15:33 -0400

    Added wait time

--- src/test/java/com/petclinic/selenium/seleniumtest/vet/VetsCreateSeleniumTest.java
+++ src/test/java/com/petclinic/selenium/seleniumtest/vet/VetsCreateSeleniumTest.java
@@ -80,4 +80,9 @@ public class VetsCreateSeleniumTest {
         driver.findElement(By.xpath("//*[@id=\"vetForm\"]/div/div[9]/div/div/input")).sendKeys("Monday, Tuesday");
         driver.findElement(By.xpath("//*[@id=\"vetForm\"]/div/div[10]/div[1]/label/input")).sendKeys(Keys.SPACE);
         driver.findElement(By.xpath("//*[@id=\"vetForm\"]/div/button")).sendKeys(Keys.SPACE);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 }
