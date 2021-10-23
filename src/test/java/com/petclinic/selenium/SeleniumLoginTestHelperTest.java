10bae9146a36ba7a7327bcca436b2bf4fd03c392
Christian C <christian.chitanu@hotmail.com>
2021-10-22 20:36:59 -0400

    added @beforeeach

--- src/test/java/com/petclinic/selenium/SeleniumLoginTestHelperTest.java
+++ src/test/java/com/petclinic/selenium/SeleniumLoginTestHelperTest.java
@@ -1,6 +1,7 @@
 package com.petclinic.selenium;
 
 import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,19 +13,24 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 @ExtendWith(SeleniumExtension.class)
 public class SeleniumLoginTestHelperTest {
     ChromeDriver driver;
+    SeleniumLoginTestHelper helper;
     public SeleniumLoginTestHelperTest(ChromeDriver driver) {
         this.driver = driver;
+
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
         System.setProperty("sel.jup.screenshot.at.the.end.of.tests", "whenfailure");
         System.setProperty("sel.jup.screenshot.format", "png");
         System.setProperty("sel.jup.output.folder", "./src/test/onFailureScreenshots/Log");
     }
+    @BeforeEach
+    void setupLogin() throws Exception {
+        this.helper = new SeleniumLoginTestHelper("LoginTestHelper", driver);
+        helper.loginTest();
+    }
     @Test
     @DisplayName("Selenium Login Test Helper Test")
     public void seleniumLoginTestHelperTest() throws Exception{
-        SeleniumLoginTestHelper helper = new SeleniumLoginTestHelper("LoginTestHelper", driver);
-        helper.loginTest();
         helper.getDriver().quit();
     }
 }
