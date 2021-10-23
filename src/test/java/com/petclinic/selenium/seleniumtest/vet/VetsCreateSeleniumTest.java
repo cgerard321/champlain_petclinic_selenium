add716af282bfc9008acbd729027607520d5717a
Louenes099 <islam.shariful@hotmail.com>
2021-10-23 05:04:02 -0400

    Created testing file

---
+++ src/test/java/com/petclinic/selenium/seleniumtest/vet/VetsCreateSeleniumTest.java
@@ -0,0 +1,21 @@
+package com.petclinic.selenium.seleniumtest.vet;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+@ExtendWith(SeleniumExtension.class)
+public class VetsCreateSeleniumTest {
