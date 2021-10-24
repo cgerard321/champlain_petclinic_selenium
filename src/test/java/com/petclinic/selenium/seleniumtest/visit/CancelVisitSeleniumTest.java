d549bf0d124ba2210cbccf1c9f3ea7292284f897
Lovepreet Bains <70494848+lbains@users.noreply.github.com>
2021-10-24 13:10:21 -0400

    test(VISE-CPC-505): selenium tests for cancel (#17)
    * Added new file for tests and started working on tests
    
    * Moved Andrian's test in a new file (CreateVisitSeleniumTest) and in same Visit folder and finished adding Cancel test did the same with Nicolas' (DeleteVisitSeleniumTest)
    
    * Added my tests in CancelVisitSeleniumTest

---
+++ src/test/java/com/petclinic/selenium/seleniumtest/visit/CancelVisitSeleniumTest.java
@@ -0,0 +1,85 @@
+package com.petclinic.selenium.seleniumtest.visit;
+
+import com.petclinic.selenium.SeleniumLoginTestHelper;
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+@ExtendWith(SeleniumExtension.class)
+public class CancelVisitSeleniumTest {
+
+    WebDriver driver;
+    SeleniumLoginTestHelper helper;
+    private final String SCREENSHOTS = "./src/test/onDemandVisitServiceScreenshots";
+
+    public CancelVisitSeleniumTest(FirefoxDriver driver) {
+        this.driver = driver;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        System.setProperty("sel.jup.screenshot.at.the.end.of.tests", "whenfailure");
+        System.setProperty("sel.jup.screenshot.format", "png");
+        System.setProperty("sel.jup.output.folder", "./src/test/failureScreenshots");
+    }
+
+    public static void takeSnapShot(WebDriver webDriver, String fileWithPath) throws Exception {
+        TakesScreenshot scrShot = ((TakesScreenshot)webDriver);
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(fileWithPath);
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    @BeforeEach
+    void setupLogin() throws Exception {
+        this.helper = new SeleniumLoginTestHelper("LoginTestHelper", driver);
+        helper.loginTest();
+    }
+
+    @Test
+    @DisplayName("trying_to_cancel_a_visit")
+    void test_cancel_visit(TestInfo testInfo) throws Exception {
+
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("navbarDropdown")));
+
+        //navigate to owners table
+        driver.findElement(By.id("navbarDropdown")).click();
+        driver.findElement(By.xpath("//a[@href='#!/owners']")).click();
+
+        //waits until Owners Row with Jean Coleman is visible and clicks it
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Jean Coleman")));
+        driver.findElement(By.linkText("Jean Coleman")).click();
+
+        //waits until the button to add a visit for the pet is visible adn then clicks it
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='#!/owners/6/pets/7/visits']")));
+        driver.findElement(By.xpath("//a[@href='#!/owners/6/pets/7/visits']")).click();
+
+        //waits for the first cancel button to be visible and then scrolls down to it and clicks it
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("table.table:nth-child(12) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(5) > button:nth-child(1)")));
+        WebElement cancel_button = driver.findElement(By.cssSelector("table.table:nth-child(12) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(5) > button:nth-child(1)"));
+        js.executeScript("arguments[0].scrollIntoView();", cancel_button);
+        cancel_button.click();
+
+        //waits until the confirm button for the confirmation is visible and clicks it
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#confirmationModalConfirmButton")));
+        driver.findElement(By.cssSelector("#confirmationModalConfirmButton")).click();
+
+        //takes a screenshot with the success notification
+        takeSnapShot(driver, SCREENSHOTS + "\\" + testInfo.getDisplayName() + "_" + System.currentTimeMillis() + ".png");
+    }
+}
--- src/test/java/com/petclinic/selenium/seleniumtest/visit/DeleteVisitSeleniumTest.java
+++ src/test/java/com/petclinic/selenium/seleniumtest/visit/DeleteVisitSeleniumTest.java
@@ -1,36 +1,28 @@
-package com.petclinic.selenium.seleniumtest;
+package com.petclinic.selenium.seleniumtest.visit;
 
-import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.sameInstance;
 
-
-@ExtendWith(SeleniumExtension.class)
-public class VISE_SeleniumTests {
+public class DeleteVisitSeleniumTest {
 
     OperaDriver driver;
     private final String SCREENSHOTS = "./src/test/onDemandScreenshots";
 
-    public VISE_SeleniumTests(OperaDriver driver) {
+    public DeleteVisitSeleniumTest(OperaDriver driver) {
         this.driver = driver;
 
         DesiredCapabilities dc = new DesiredCapabilities();
@@ -51,99 +43,6 @@ public class VISE_SeleniumTests {
         //Copy file at destination
         FileUtils.copyFile(SrcFile, DestFile);
     }
-    @Test
-    @DisplayName("test_create_a_visit")
-    void test_create_a_visit(TestInfo testInfo) throws Exception {
-
-        //wait max 5 seconds before a timeout
-        WebDriverWait wait=new WebDriverWait(driver, 5);
-
-        //setting up path and window size
-        driver.get("http://localhost:8080");
-        driver.manage().window().maximize();
-
-        //enter authentication information
-        driver.findElement(By.id("email")).sendKeys("admin");
-        driver.findElement(By.id("pwd")).sendKeys("admin");
-        driver.findElement(By.id("button")).click();
-
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Owners")));
-
-
-
-
-
-                //navigation to visits page
-        driver.findElement(By.linkText("Owners")).click();
-
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul[1]/li[4]/div/a[1]")));
-        WebElement a1 = driver.findElement(By.xpath("//a[@href='#!/owners']"));
-        a1.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Betty Davis"))); //Will crash here if not wait
-        a1 = driver.findElement(By.linkText("Betty Davis"));
-        a1.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Add Visit")));
-        a1 = driver.findElement(By.linkText("Add Visit"));
-        a1.click();
-
-
-
-
-
-        //Selecting the vet : "Helen"
-
-        TimeUnit.SECONDS.sleep(2);//special case because it's a textbox
-
-        driver.findElement(By.xpath("//*[@id=\"selectedVet\"]/option[1]")).click();
-        TimeUnit.SECONDS.sleep(2);
-        String selectedVet = driver.findElement(By.xpath("//*[@id=\"selectedVet\"]/option[3]")).getText();
-        System.out.println(selectedVet +" : vetname");
-        assertThat(selectedVet, is("Helen Leary"));
-
-
-            // Necessary scroll
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,250)", "");
-
-        driver.findElement(By.xpath("//*[@id=\"selectedVet\"]/option[3]")).click();
-         TimeUnit.SECONDS.sleep(1);
-
-
-            //typing TestVisitDescription inside the text box labelled "description"
-
-        driver.findElement(By.xpath("//*[@id=\'description_textarea\']")).sendKeys("TestVisitDescription");
-
-
-
-       driver.findElement(By.id("submit_button")).click();
-       wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("confirmationModalConfirmButton")));
-
-       driver.findElement(By.id("confirmationModalConfirmButton")).click();
-
-            //For console display purposes
-        String testname = "TestVisitDescription";
-
-            // Checks the document if the string is part of it.
-
-        if ( driver.getPageSource().contains("TestVisitDescription")){
-            System.out.println("Text: " + testname + " was created successfully. ");
-            assertThat( driver.getPageSource().contains("TestVisitDescription"), is(Boolean.TRUE));
-        } else {
-            System.out.println("Text: " + testname + " was NOT created successfully. ");
-
-        }
-
-
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.quit();
-    }
-
 
     @Test
     @DisplayName("test_delete_a_visit")
