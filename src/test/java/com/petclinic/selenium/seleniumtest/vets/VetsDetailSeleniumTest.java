1945414965781f1e85c1dce0bdaaaf47907baee9
yanak <yana.kapeluhova@gmail.com>
2021-10-25 00:47:57 -0400

    Cleaned VetsDetailsSeleniumTest.java

--- src/test/java/com/petclinic/selenium/seleniumtest/vets/VetsDetailSeleniumTest.java
+++ src/test/java/com/petclinic/selenium/seleniumtest/vets/VetsDetailSeleniumTest.java
@@ -4,24 +4,21 @@ import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @ExtendWith(SeleniumExtension.class)
 public class VetsDetailSeleniumTest {
-    ChromeDriver driver;
     private final String SCREENSHOTS = "./src/test/onDemandScreenshots";
+    ChromeDriver driver;
 
     public VetsDetailSeleniumTest(ChromeDriver driver) {
         this.driver = driver;
@@ -45,7 +42,7 @@ public class VetsDetailSeleniumTest {
         FileUtils.copyFile(SrcFile, DestFile);
     }
 
-    void Login(){
+    void Login() {
         driver.get("http://localhost:8080");
         driver.manage().window().maximize();
 
