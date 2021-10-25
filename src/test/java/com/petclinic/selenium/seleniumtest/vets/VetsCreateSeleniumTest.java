dbf709441047abe95935f7145c20c357a5146911
yanak <yana.kapeluhova@gmail.com>
2021-10-25 00:47:11 -0400

    Cleaned VetsCreateSeleniumTEst.java

--- src/test/java/com/petclinic/selenium/seleniumtest/vets/VetsCreateSeleniumTest.java
+++ src/test/java/com/petclinic/selenium/seleniumtest/vets/VetsCreateSeleniumTest.java
@@ -4,19 +4,17 @@ import io.github.bonigarcia.seljup.SeleniumExtension;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.Select;
 
 import java.io.File;
-import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+
 /*
 This test was prepared by Shariful Islam
 It's testing UI Creation of a VET
@@ -24,8 +22,8 @@ IMPORTANT NOTICE: This test only works once as soon as the system is up as it te
  */
 @ExtendWith(SeleniumExtension.class)
 public class VetsCreateSeleniumTest {
-    ChromeDriver driver;
     private final String SCREENSHOTS = "./src/test/onDemandScreenshots";
+    ChromeDriver driver;
 
     public VetsCreateSeleniumTest(ChromeDriver driver) {
         this.driver = driver;
